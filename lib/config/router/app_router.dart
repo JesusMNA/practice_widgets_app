5cbc626e60fcfda3c42a2137773a805a43f42a2d
Jesús Návar Aragón <jesuso_0709@hotmail.com>
2023-04-07 00:44:09 -0600

    created screens

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -18,4 +18,34 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
     name: CardsScreen.name,
     builder: (context, state) => const CardsScreen(),
   ),
+  GoRoute(
+    path: '/animated',
+    name: AnimatedScreen.name,
+    builder: (context, state) => const AnimatedScreen(),
+  ),
+  GoRoute(
+    path: '/app_tutorial',
+    name: AppTutorialScreen.name,
+    builder: (context, state) => const AppTutorialScreen(),
+  ),
+  GoRoute(
+    path: '/infinite_scroll',
+    name: InfiniteScrollScreen.name,
+    builder: (context, state) => const InfiniteScrollScreen(),
+  ),
+  GoRoute(
+    path: '/progress',
+    name: ProgressScreen.name,
+    builder: (context, state) => const ProgressScreen(),
+  ),
+  GoRoute(
+    path: '/snackbar',
+    name: SnackbarScreen.name,
+    builder: (context, state) => const SnackbarScreen(),
+  ),
+  GoRoute(
+    path: '/ui_controls',
+    name: UiControlsScreen.name,
+    builder: (context, state) => const UiControlsScreen(),
+  ),
 ]);
